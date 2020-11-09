90de1c8c9e96d5a2fe70c7e3f18e697d9af300fb
Kristine Horton <kristinehrtn@yahoo.com>
2020-11-09 16:23:05 -0700

    commit all changes

---
+++ script.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    function start() {
+
+    
+      navigator.geolocation.getCurrentPosition(success,error);
+          function success (pos) {
+              console.log(pos);
+        let lat=pos.coords.latitude;
+        let long=pos.coords.longitude;
+        weather(lat, long);
+          }
+        }
+        start()
+    
+    $("btn").on("click", function(event){
+        event.preventDefault();
+        let city=$(".form-control").val();
+        cityWeather(city);
+        console.log(city);
+                })
+    function error() {
+        console.log("error");
+    }
+function weather(lat, long) {
+    let url=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=230b9b804f5520f3d52810d59c91b1b0`;
+    console.log(url);
+    $.ajax({url: url, type:"GET"}).done(function(data) {
+        console.log("ajax");
+        updateDOM(data);  
+})
+};
+            function updateDOM(data) {
+        let city=data.name;
+        console.log(data)
+        let temp=Math.round(data.current.temp);
+        const celsius = temp - 273;
+        let fahrenheit = Math.floor(celsius * (9/5) + 32);
+        let desc=data.current.weather[0].description;
+        let icon=data.current.weather[0].icon;
+
+
+        $("#city").html(city);
+        $("#temp").html(fahrenheit);
+        $("#desc").html(desc);
+        $("#icon").attr("src", icon);
+    }
+function cityWeather(city) {
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=230b9b804f5520f3d52810d59c91b1b0`;
+    console.log(url);
+    $.ajax({url: url, type:"GET"}).done(function(data) {
+        console.log("ajax");
+        updateDOM(data);    
+})
+}
+});
